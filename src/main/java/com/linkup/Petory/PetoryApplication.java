4ce2b2c44e879f267ed49da62f01c6b5d099bed4
pyb <wowong123@naver.com>
2025-10-28 17:56:15 +0900

    프로젝트 초기 설정

--- src/main/java/com/linkup/Petory/PetoryApplication.java
+++ src/main/java/com/linkup/Petory/PetoryApplication.java
@@ -2,8 +2,10 @@ package com.linkup.Petory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationProperties(prefix = "app")
 public class PetoryApplication {
 
 	public static void main(String[] args) {
