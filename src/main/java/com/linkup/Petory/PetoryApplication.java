270fba44198ca0c6bc6a213147134f847f50eab5
pyb <wowong123@naver.com>
2025-10-28 17:51:42 +0900

    프로젝트 셋팅

---
+++ src/main/java/com/linkup/Petory/PetoryApplication.java
@@ -0,0 +1,13 @@
+package com.linkup.Petory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkup/Petory/PetoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkup.Petory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
